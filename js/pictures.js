67e6e7b616da00fe05c6a52c5609f3b779eefa38
Stan Chashchnikov <chashchnikov@gmail.com>
2016-02-03 13:19:52 +0300

    Загрузка данных и вывод их с помощью шаблона

---
+++ js/pictures.js
@@ -0,0 +1,53 @@
+'use strict';
+(function() {
+  var filters = document.querySelector('.filters');
+  var pictures = document.querySelector('.pictures');
+
+  filters.classList.add('hidden');
+
+  var picturesFragment = document.createDocumentFragment();
+
+  window.pictures.forEach(function(picture) {
+    picturesFragment.appendChild(getElementFromTemplate(picture));
+  });
+  pictures.appendChild(picturesFragment);
+
+  function getElementFromTemplate(picture) {
+    var template = document.querySelector('#picture-template');
+    var element = ('content' in template) ? template.content.children[0].cloneNode(true) : template.children[0].cloneNode(true);
+
+    var templateImage = element.querySelector('img');
+    var IMAGE_TIMEOUT = 10000;
+
+    if (element.classList.contains('picture')) {
+      element.href = picture.url;
+      element.querySelector('.picture-comments').textContent = picture.comments;
+      element.querySelector('.picture-likes').textContent = picture.likes;
+
+      var img = new Image();
+
+      img.onload = function() {
+        clearTimeout(imageLoadTimeout);
+        img.width = 182;
+        img.height = 182;
+        element.replaceChild(img, templateImage);
+      };
+
+      img.onerror = function() {
+        imageLoadFailure();
+      };
+
+      var imageLoadTimeout = setTimeout(function() {
+        imageLoadFailure();
+      }, IMAGE_TIMEOUT);
+
+      img.src = picture.url;
+
+    }
+    return element;
+
+    function imageLoadFailure() {
+      element.classList.add('picture-load-failure');
+    }
+  }
+})();
