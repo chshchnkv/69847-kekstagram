8cabc3e51fe48d950144b580761a92eb79ce1569
Stan Chashchnikov <chashchnikov@gmail.com>
2016-02-19 01:35:21 +0300

    Основное задание + первое дополнительное

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,15 @@
 'use strict';
 (function(gl) {
   function Gallery() {
+    this._data = null;
+    this._currentPicture = 0;
+
     this.element = document.querySelector('.gallery-overlay');
     this._closeButton = this.element.querySelector('.gallery-overlay-close');
     this._image = this.element.querySelector('.gallery-overlay-image');
+    this._likes = this.element.querySelector('.likes-count');
+    this._comments = this.element.querySelector('.comments-count');
+
     this._onPhotoClick = this._onPhotoClick.bind(this);
     this._onCloseClick = this._onCloseClick.bind(this);
     this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
@@ -13,17 +19,18 @@
     this.element.classList.remove('invisible');
     this._image.addEventListener('click', this._onPhotoClick);
     this._closeButton.addEventListener('click', this._onCloseClick);
-    document.addEventListener('keypress', this._onDocumentKeyDown);
+    document.addEventListener('keydown', this._onDocumentKeyDown);
   };
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
     this._image.removeEventListener('click', this._onPhotoClick);
     this._closeButton.removeEventListener('click', this._onCloseClick);
-    document.removeEventListener('keypress', this._onDocumentKeyDown);
+    document.removeEventListener('keydown', this._onDocumentKeyDown);
   };
 
   Gallery.prototype._onPhotoClick = function() {
+    this.nextPicture();
   };
 
   Gallery.prototype._onCloseClick = function() {
@@ -31,9 +38,34 @@
   };
 
   Gallery.prototype._onDocumentKeyDown = function(event) {
-    if (event.keyCode === 27) {
-      this.hide();
+    switch (event.keyCode) {
+      case 27: this.hide(); break;
+      case 37: this.previousPicture(); break;
+      case 39: this.nextPicture(); break;
     }
+  };
+
+  Gallery.prototype.setPictures = function(data) {
+    this._data = data;
+  };
+
+  Gallery.prototype.setCurrentPicture = function(pictureNumber) {
+    if ((pictureNumber >= 0) && (pictureNumber < this._data.length)) {
+      this._currentPicture = pictureNumber;
+      var photo = this._data[pictureNumber];
+
+      this._image.src = photo.getImageSrc();
+      this._comments.textContent = photo.getComments();
+      this._likes.textContent = photo.getLikes();
+    }
+  };
+
+  Gallery.prototype.nextPicture = function() {
+    this.setCurrentPicture(this._currentPicture + 1);
+  };
+
+  Gallery.prototype.previousPicture = function() {
+    this.setCurrentPicture(this._currentPicture - 1);
   };
 
   gl.Gallery = Gallery;
--- js/photo.js
+++ js/photo.js
@@ -3,6 +3,7 @@
   function Photo(data) {
     this._data = data;
     this.element = null;
+    this._onClick = this._onClick.bind(this);
   }
 
   Photo.prototype.getLikes = function() {
@@ -17,7 +18,11 @@
     return (this._data ? new Date(this._data.date) : new Date());
   };
 
-  Photo.prototype.render = function() {
+  Photo.prototype.getImageSrc = function() {
+    return (this._data ? this._data.url : '');
+  };
+
+  Photo.prototype.render = function(appendTo) {
     var template = document.querySelector('#picture-template');
     this.element = ('content' in template) ? template.content.children[0].cloneNode(true) : template.children[0].cloneNode(true);
 
@@ -47,13 +52,39 @@
 
       img.src = this._data.url;
 
+      this.element.addEventListener('click', this._onClick);
+
     }
+
+    if (appendTo) {
+      appendTo.appendChild(this.element);
+    }
+
     return this.element;
+  };
+
+  Photo.prototype.remove = function(from) {
+    this.element.removeEventListener('click', this._onClick);
+    if (from) {
+      from.removeChild(this.element);
+    }
   };
 
   Photo.prototype._imageLoadFailure = function() {
     this.element.classList.add('picture-load-failure');
   };
+
+  Photo.prototype._onClick = function(event) {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG' &&
+       !this.element.classList.contains('picture-load-failure')) {
+      if (typeof this.onClick === 'function') {
+        this.onClick();
+      }
+    }
+  };
+
+  Photo.prototype.onClick = null;
 
   gl.Photo = Photo;
 })(window);
